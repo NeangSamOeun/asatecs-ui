2fd2a21ebc57ee44db0efc961ca14af6f71054bb
Neang SamOeurn <samoeunneang600@gmail.com>
2025-08-08 23:47:50 +0700

    update

--- src/app/components/home-component/home/home.component.ts
+++ src/app/components/home-component/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,24 +10,21 @@ export class HomeComponent {
 
   images: any[] = [
     {
-      itemImageSrc: 'https://ps.w.org/ml-slider/assets/banner-1544x500.png?rev=2907610',
-      thumbnailImageSrc: 'assets/images/thumb1.jpg',
+      itemImageSrc: 'assets/images/slider/img1.png',
       alt: 'Description 1',
       title: 'Title 1'
     },
     {
-      itemImageSrc: 'https://ps.w.org/image-slider-widget/assets/banner-772x250.png?rev=1674939',
-      thumbnailImageSrc: 'assets/images/thumb2.jpg',
+      itemImageSrc: 'assets/images/slider/img2.png',
       alt: 'Description 2',
       title: 'Title 2'
     },
-    // Add more objects as needed
   ];
 
   responsiveOptions: any[] = [
-    { breakpoint: '1024px', numVisible: 5 },
-    { breakpoint: '768px', numVisible: 3 },
-    { breakpoint: '560px', numVisible: 1 }
+    {breakpoint: '1024px', numVisible: 3},
+    {breakpoint: '768px', numVisible: 2},
+    {breakpoint: '560px', numVisible: 1}
   ];
 
 
